e29132e3eb835195e37f8391f1218b9d3f9075a2
faikmermer <61328016+faikmermer@users.noreply.github.com>
2023-07-14 22:59:20 +0300

    Exercise Solution

---
+++ Unit6/Exercise6-28.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define SIZE 20
+
+void yazdir(int array[]);
+
+int main(void){
+
+    int max = 20;
+    int min = 1;
+    srand(time(NULL));
+    int binarySearch[SIZE] = {0};
+    int element;
+
+    for(size_t i = 0; i < SIZE; i++){
+        element = rand() % (max + min - 1) + min;
+        binarySearch[i] = element;
+        if(binarySearch[1] > 0){
+            for(size_t j = 0; j < i; j++){
+                if(binarySearch[j] == binarySearch[i]){
+                    binarySearch[i] = 0;
+                    --i;
+                    break;
+                }
+            }
+        }
+
+    }
+    yazdir(binarySearch);
+
+
+
+}
+
+void yazdir(int array[]){
+    printf("%s","{");
+   for(size_t i = 0; i < SIZE; i++){
+        printf("%4d", array[i]);
+   }
+   printf("%s", " }");
+}
